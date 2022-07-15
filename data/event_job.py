6b5bdac7e929ebe3b67b0d15e02dc531cb3b61dd
ekzm8523 <mjw8523@gmail.com>
2022-07-15 15:06:16 +0900

    🔨 remove main start code

--- data/event_job.py
+++ data/event_job.py
@@ -8,7 +8,3 @@ def upload_unlabeled_data_to_label_studio_from_air_table() -> None:
     label_studio = LabelStudioAPI(settings.LABEL_STUDIO_URL, settings.LABEL_STUDIO_ACCESS_TOKEN)
     unlabeled_dataset = get_required_labelling_record()
     label_studio.import_tasks(unlabeled_dataset)
-
-
-if __name__ == '__main__':
-    upload_unlabeled_data_to_label_studio_from_air_table()
