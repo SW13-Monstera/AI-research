00c01b676ff0a9d3d1bf2bc0b51a7327704108a0
ekzm8523 <mjw8523@gmail.com>
2022-09-01 18:40:04 +0900

    :tada: start prompt tuning
    implement validate klue nli dataset and get pretrained model as google mt5 base model

--- prompt_tuning/train.py
+++ prompt_tuning/train.py
@@ -1,5 +1,41 @@
+import random
+
+from openprompt.data_utils import InputExample
+from openprompt.plms import load_plm
+
+from core.utils import seed_everything
 from datasets import load_dataset
 
 if __name__ == "__main__":
-    dataset = load_dataset("klue", "nli")
-    prompt_input_dataset = {}
+    seed = 42
+    task = "nli"
+
+    seed_everything(seed)
+    dataset = load_dataset("klue", task)
+    prompt_input_dataset = {"train": [], "validation": [], "test": []}
+    for data in dataset["train"]:  # train data
+        input_example = InputExample(
+            text_a=data["premise"],
+            text_b=data["hypothesis"],
+            label=data["label"],
+            guid=data["guid"],
+        )
+        prompt_input_dataset["train"].append(input_example)
+
+    validation_dataset = list(dataset["validation"])
+    random.seed(seed)
+    random.shuffle(validation_dataset)
+
+    for i, data in enumerate(validation_dataset):
+        input_example = InputExample(
+            text_a=data["premise"],
+            text_b=data["hypothesis"],
+            label=data["label"],
+            guid=data["guid"],
+        )
+        if i % 2 == 0:
+            prompt_input_dataset["validation"].append(input_example)
+        else:
+            prompt_input_dataset["test"].append(input_example)
+    pretrain_model_path = "google/mt5-base"
+    plm, tokenizer, model_config, WrapperClass = load_plm("t5", pretrain_model_path)
