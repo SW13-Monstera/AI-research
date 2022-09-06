821858eead208f508a14386e12f778a2199ffdaf
ekzm8523 <mjw8523@gmail.com>
2022-09-06 14:59:36 +0900

    :recycle: Prompt Tuning Training code refactoring
    prompt tuning training code refactoring

--- prompt_tuning/dataset.py
+++ prompt_tuning/dataset.py
@@ -1,29 +1,45 @@
 import random
+from typing import Tuple
 
+from openprompt import PromptDataLoader
 from openprompt.data_utils import InputExample
+from openprompt.prompts import ManualTemplate
+from transformers import PreTrainedTokenizer
 
 from datasets import load_dataset
 
 
-class PromptPreprocessor:
+class PromptDataModule:
     def __init__(
         self,
         dataset_path: str = "klue",
         dataset_name: str = "nli",
         seed: int = 42,
         split_rate: tuple = (8, 1, 1),  # train, val, test
-    ):
-        self.dataset = load_dataset(dataset_path, dataset_name)
+        max_seq_length: int = 512,
+        decoder_max_length: int = 3,
+        batch_size: int = 4,
+        teacher_forcing: bool = False,
+        predict_eos_token: bool = False,
+        truncate_method: str = "head",
+        shuffle: bool = False,
+    ) -> None:
         self.seed = seed
-        self.prompt_input_dataset = self.convert_to_prompt_input_dataset(split_rate)
+        self.max_seq_length = max_seq_length
+        self.decoder_max_length = decoder_max_length
+        self.batch_size = batch_size
+        self.teacher_forcing = teacher_forcing
+        self.predict_eos_token = predict_eos_token
+        self.truncate_method = truncate_method
+        self.shuffle = shuffle
+        self.dataset = load_dataset(dataset_path, dataset_name)
+        self.prompt_input_dataset = self._convert_to_prompt_input_dataset(split_rate)
 
-    def convert_to_prompt_input_dataset(
-        self, split_rate: tuple, shuffle: bool = False
-    ) -> dict:
+    def _convert_to_prompt_input_dataset(self, split_rate: tuple) -> dict:
         prompt_input_dataset = {"train": [], "val": [], "test": []}
-        every_dataset = list(self.dataset["train"]) + list(self.dataset["val"])
+        every_dataset = list(self.dataset["train"]) + list(self.dataset["validation"])
 
-        if shuffle:
+        if self.shuffle:
             random.seed(self.seed)
             random.shuffle(every_dataset)
 
@@ -48,3 +64,27 @@ class PromptPreprocessor:
                 prompt_input_dataset["test"].append(input_example)
 
         return prompt_input_dataset
+
+    def get_data_loader(
+        self,
+        template: ManualTemplate,
+        tokenizer: PreTrainedTokenizer,
+        tokenizer_wrapper_class,
+    ) -> Tuple[PromptDataLoader, PromptDataLoader, PromptDataLoader]:
+        train_data_loader, val_data_loader, test_data_loader = [
+            PromptDataLoader(
+                dataset=self.prompt_input_dataset[data_type],
+                template=template,
+                tokenizer=tokenizer,
+                tokenizer_wrapper_class=tokenizer_wrapper_class,
+                max_seq_length=self.max_seq_length,
+                decoder_max_length=self.decoder_max_length,
+                batch_size=self.batch_size,
+                shuffle=self.shuffle,
+                teacher_forcing=self.teacher_forcing,
+                predict_eos_token=self.predict_eos_token,
+                truncate_method=self.truncate_method,
+            )
+            for data_type in ["train", "val", "test"]
+        ]
+        return train_data_loader, val_data_loader, test_data_loader
--- prompt_tuning/loss.py
+++ prompt_tuning/loss.py
@@ -1,4 +1,11 @@
-def get_loss(inputs, prompt_model, criterion, device):
+from openprompt import PromptForClassification
+from openprompt.data_utils import InputFeatures
+from torch import Tensor
+from torch.nn.modules import Module
+
+
+def get_loss(inputs: InputFeatures, prompt_model: PromptForClassification, criterion: Module) -> Tensor:
+    device = prompt_model.device
     inputs = inputs.to(device)
     logits = prompt_model(inputs)
     labels = inputs.label
