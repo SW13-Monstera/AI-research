6c9af83893fdc20a9b12785ef4cc53848a168bb9
ekzm8523 <mjw8523@gmail.com>
2022-09-14 00:56:35 +0900

    :sparkles: Add load user answer dataset logic
    add load user answer dataset logic and aplit dataset logic

--- prompt_tuning/dataset.py
+++ prompt_tuning/dataset.py
@@ -1,13 +1,27 @@
 import random
 from collections import defaultdict
-from typing import Tuple
+from typing import List, Tuple
 
+import numpy as np
+import pandas as pd
 from openprompt import PromptDataLoader
 from openprompt.data_utils import InputExample
 from openprompt.prompts import ManualTemplate
+from pydantic import BaseModel
 from transformers import PreTrainedTokenizer
 
 from datasets import load_dataset
+
+
+class RequiredGradingData(BaseModel):
+    problem_id: str
+    answer_id: int
+    guid: str
+    source: str
+    premise: str
+    hypothesis: str
+    label: int
+    criterion_idx: int
 
 
 class PromptDataModule:
@@ -89,3 +103,67 @@ class PromptDataModule:
             for data_type in ["train", "val", "test"]
         ]
         return train_data_loader, val_data_loader, test_data_loader
+
+
+def load_labeled_dataset(csv_path: str = "../static/labeled_dataset.csv") -> List[RequiredGradingData]:
+    df = pd.read_csv(csv_path)
+    print(f"pre data size : {len(df)}")
+    df["user_answer"] = df["user_answer"].apply(
+        lambda x: x.strip().replace("\n", "").replace("\xa0", "").replace("  ", " ")
+    )
+    df["user_answer"].replace("", np.nan, inplace=True)
+    df.dropna(axis=0, subset=["user_answer"], inplace=True)  # 빈 답변 제거
+    print(f"after data size : {len(df)}")
+
+    if isinstance(df["correct_scoring_criterion"][0], str):  # list를 string으로 표현된 경우 type casting
+        df["correct_scoring_criterion"] = df["correct_scoring_criterion"].apply(eval)
+    if isinstance(df["scoring_criterion"][0], str):
+        df["scoring_criterion"] = df["scoring_criterion"].apply(eval)
+
+    dataset = []
+    for row_data in df.itertuples():
+        for i, criterion in enumerate(row_data.scoring_criterion):
+            data = RequiredGradingData(
+                problem_id=row_data.problem_id,
+                answer_id=row_data.Index if isinstance(row_data.Index, str) else str(row_data.Index),
+                guid=f"{row_data.Index}-{i}",
+                source="CS-broker",
+                premise=row_data.user_answer,
+                hypothesis=criterion,
+                label=1 if criterion in row_data.correct_scoring_criterion else 0,
+                criterion_idx=i,
+            )
+            dataset.append(data)
+    return dataset
+
+
+def split_dataset(
+    dataset: List[RequiredGradingData], shuffle: bool = False, seed: int = 42, split_rate: Tuple = (8, 1, 1)
+) -> Tuple[List[RequiredGradingData], List[RequiredGradingData], List[RequiredGradingData]]:
+    answer_id_list = list(set((data.answer_id for data in dataset)))
+    if shuffle:
+        random.seed(seed)
+        random.shuffle(answer_id_list)
+    split_rate_total = sum(split_rate)
+    start_val_idx = int(split_rate[0] / split_rate_total * len(answer_id_list))
+    start_test_idx = start_val_idx + int(split_rate[1] / split_rate_total * len(answer_id_list))
+    train_answer_id_set = set(answer_id_list[:start_val_idx])
+    val_answer_id_set = set(answer_id_list[start_val_idx:start_test_idx])
+    test_answer_id_set = set(answer_id_list[start_test_idx:])
+
+    assert len(train_answer_id_set) + len(val_answer_id_set) + len(test_answer_id_set) == len(
+        answer_id_list
+    ), f"dataset split error: split된 데이터와 전체 데이터의 갯수가 맞지 않습니다. {__file__}을 확인해주세요"
+
+    train_dataset, val_dataset, test_dataset = [], [], []
+
+    for data in dataset:
+        if data.answer_id in train_answer_id_set:
+            train_dataset.append(data)
+        elif data.answer_id in val_answer_id_set:
+            val_dataset.append(data)
+        elif data.answer_id in test_answer_id_set:
+            test_dataset.append(data)
+        else:
+            assert f"split error: dataset split 도중 에러가 발생하였습니다. {__file__}을 확인해주세요"
+    return train_dataset, val_dataset, test_dataset
